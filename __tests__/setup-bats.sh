2451f7c6e9fd67528dbbc100727c501dd0313222
Sebastian Hjelm <melon.bag@hotmail.com>
2024-06-24 17:16:20 +0200

    Use latest bats

--- __tests__/setup-bats.sh
+++ __tests__/setup-bats.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:duggan/bats
+# sudo add-apt-repository ppa:duggan/bats
 sudo apt-get update
-sudo apt-get install bats
+# sudo apt-get install bats
 sudo apt-get install git
 sudo apt-get install tree
+
+git clone https://github.com/bats-core/bats-core.git
+cd bats-core
+sudo ./install.sh /usr/local
